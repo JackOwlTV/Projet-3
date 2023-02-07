c1d4bb54676ad818a48914ed2a6ba92a8f6ed62d
JackOwlTV <dylan.vivant@sfr.fr>
2023-02-07 09:30:34 +0100

    Filtrage v1

--- FrontEnd/works.js
+++ FrontEnd/works.js
@@ -1,8 +1,18 @@
+// récupération de l'api
+let figures = [];
+let elementsFilter = [];
+let all = [];
+let objects = [];
+let appartements = [];
+let restaurants = [];
+
+function pageload(){
+  
 fetch('http://localhost:5678/api/works')
 
   .then(response => response.json())
   .then(data => {
-
+// génération des projets via l'api
     for (let i = 0; i < data.length; i++) {
 
         let figure = document.createElement("figure");
@@ -10,10 +20,12 @@ fetch('http://localhost:5678/api/works')
         let figcaption = document.createElement("figcaption");
         let gallery = document.querySelector(".gallery");
 
-        figure.setAttribute("data-category-id", data[i].categoryId);
+        figure.setAttribute("category-", data[i].categoryId);
         img.setAttribute("src", data[i].imageUrl);
         img.setAttribute("crossorigin", "anonymous");
         figcaption.innerHTML = data[i].title;
+
+        figures.push(figure);
 
         gallery.appendChild(figure);
         figure.appendChild(img);
@@ -22,5 +34,52 @@ fetch('http://localhost:5678/api/works')
 
     }
 })
-.catch(error => console.error(error))
+.catch(error => console.error(error));
 
+
+fetch('http://localhost:5678/api/categories')
+  .then(response => response.json())
+  .then(data => {
+    const filtres = document.querySelector('.filtres');
+    for (let i = 0; i < data.length; i++) {
+      const category = data[i];
+      const button = document.createElement('button');
+      button.id = 'category-' + (i + 1);
+      button.classList.add = ('elements-filter')
+      button.innerText = category.name;
+      filtres.appendChild(button);
+    }
+  });
+
+
+// Variable Filtres
+elementsFilter =  document.querySelectorAll(".elements-filter");
+all = document.querySelector(".all");
+objects = document.querySelector(".objects-button");
+appartements = document.querySelector(".appartements-button");
+restaurants = document.querySelector(".restaurants-button");
+
+//Filtre
+
+for (let element of elementsFilter) {
+  element.addEventListener("click", function(){
+    console.log(figures)
+    for ( let e of elementsFilter) {
+      e.classList.remove("active");
+    }
+    this.classList.add("active");
+    for (let figure of figures) {
+      if (
+        figure.getAttribute("data-category-id") === this.getAttribute("data-category-id")
+      ){
+        console.log('même catégory')
+        console.log(figure.getAttribute('data-category-id'))
+        console.log(this.getAttribute('data-category-id'))
+        figure.style.display = "block";} 
+      else if (this === all){
+        figure.style.display = "block";} 
+      else {figure.style.display = "none";}}
+  })
+}
+}
+document.addEventListener('DOMContentLoaded', pageload, false);
