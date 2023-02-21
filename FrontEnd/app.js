86c1db66a4636fbac240538839858fc466b6cf96
JackOwlTV <dylan.vivant@sfr.fr>
2023-02-21 10:22:48 +0100

    modal - galerie photo v1
    Visuel pratiquement fini, génération des figures ok, de même pour l'apparition de la modal et sa fermeture. Manque les icons de suppression des figures, ainsi que l'icon de changement d'ordre des dites figures.

--- FrontEnd/app.js
+++ FrontEnd/app.js
@@ -4,5 +4,33 @@ const modalTriggers = document.querySelectorAll(".modal-trigger");
 modalTriggers.forEach(trigger => trigger.addEventListener("click", toggleModal));
 
 function toggleModal(){
+    fetch('http://localhost:5678/api/works')
+
+        .then(response => response.json())
+        .then(data => {
+    // génération des projets via l'api
+        for (let i = 0; i < data.length; i++) {
+
+            let figure = document.createElement("figure");
+            let img = document.createElement("img"); 
+            
+            let figcaption = document.createElement("figcaption");
+            let galerie = document.querySelector(".galerie");
+
+            figure.setAttribute("category-", data[i].categoryId);
+            img.classList.add("edit")
+            img.setAttribute("src", data[i].imageUrl);
+            img.setAttribute("crossorigin", "anonymous");
+            figcaption.textContent = "éditer";
+
+            figures.push(figure);
+
+            galerie.appendChild(figure);
+            figure.appendChild(img);
+            figure.appendChild(figcaption);
+
+        }
+    })
+    .catch(error => console.error(error));
     modalContainer.classList.toggle("actives")
 }
