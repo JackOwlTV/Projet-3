caa561de6d07e1464c9a692f2973268de4ae8c95
JackOwlTV <dylan.vivant@sfr.fr>
2023-03-02 19:35:34 +0100

    suppression all

--- FrontEnd/app.js
+++ FrontEnd/app.js
@@ -11,9 +11,23 @@ if (userToken) {
 
 const modalContainer = document.querySelector(".modal-container");
 const modalTriggers = document.querySelectorAll(".modal-trigger");
+const modalDelete = document.querySelector(".delete-works-modal");
+const modalAdd = document.querySelector(".add-works-modal");
+const deletAllWorksBtn = document.querySelector(".delete-all-works");
 
+const modalReturn = document.querySelector(".fa-arrow-left");
+const openModalAdd = document.querySelector(".add-works");
 
 modalTriggers.forEach(trigger => trigger.addEventListener("click", toggleModal));
+openModalAdd.addEventListener("click", function(){
+    modalAdd.classList.remove("inactif");
+    modalDelete.classList.add("inactif");
+})
+
+modalReturn.addEventListener("click", function(){
+    modalAdd.classList.add("inactif");
+    modalDelete.classList.remove("inactif");
+})
 
 let trashIcons = [];
 
@@ -46,9 +60,9 @@ fetch('http://localhost:5678/api/works')
         figure.appendChild(figcaption);
 
     }
-
-    figureDelete();
-})
+        initDeleteWorks();
+/*     figureDelete();
+ */})
 .catch(error => console.error(error));
 
 function toggleModal(){
@@ -63,11 +77,11 @@ function toggleModal(){
                 modalContainer.classList.add("actives");
             }  
         })
-}
+    }
 }
     
 
-function figureDelete(){
+/* function figureDelete(){
 
 for (let trash of trashIcons) {
     trash.addEventListener("click", function () {
@@ -86,9 +100,66 @@ for (let trash of trashIcons) {
             }
         }
     });
+} */
+function initDeleteWorks() {
+    // pour un travail
+    for (let trash of trashIcons) {
+      trash.addEventListener("click", function () {
+        const workId = trash.getAttribute("data-id");
+        deleteWork(workId);
+      });
+    }
+  
+    // pour tous les travaux
+    deletAllWorksBtn.addEventListener("click", async function () {
+      if (confirm("Êtes-vous sûr de vouloir supprimer tous les travaux ?")) {
+        try {
+          for (let i in data) {
+            const workId = data[i].id;
+            deleteWork(workId);
+          }
+          galerie.innerHTML = "";
+          gallery.innerHTML = "";
+          console.log("Tous les travaux ont été supprimés");
+          if (!response.ok) {
+            throw new Error("Erreur lors de la suppression des éléments");
+          }
+        } catch (error) {
+          console.error(error);
+        }
+      }
+    });
 }
 
-}
+async function deleteWork(workId) {
+    try {
+      const fetchInit = {
+        method: "DELETE",
+        headers: {
+          Authorization: `Bearer ${userToken}`,
+        },
+      };
+  
+      const response = await fetch(
+        `http://localhost:5678/api/works/${workId}`,
+        fetchInit
+      );
+      if (response.ok) {
+        const figures = document.querySelectorAll("figure");
+        for (let figure of figures) {
+          if (figure.getAttribute("data-id") === workId) {
+            figure.remove();
+            console.log(
+              `${figure.querySelector("img").alt} a été supprimé avec succès`
+            );
+          }
+        }
+      } else throw new Error("Erreur lors de la suppression de l'élément");
+    } catch (error) {
+      console.error(error);
+    }
+  }
+
 
 /* Si l'on click sur le bouton ajouter une photo cela fais apparaitre la seconde modal
     Changer l'input categorie par l'élément pertinant (demander au mentor ou l'élève)
